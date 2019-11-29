11f9774f019fd7a03906788f25c650449c31ec75
Lucas Calheiros <lucaslc@ic.ufal.br>
2019-11-29 09:49:45 -0300

    autostart cam, register columns order

--- gsheets.js
+++ gsheets.js
@@ -250,7 +250,7 @@ function valSel() {
       "range": 'Registros!A1:G',  //Set this to cell want to add 'x' to.
         "majorDimension": "ROWS",
         "values": [
-          [dataAtualFormatada(), 'FARMACIA INTERNA', 'SAIDA', medName, seriesName, valDate, value]]
+          ['','SAIDA', dataAtualFormatada(), 'FARMACIA INTERNA (CONSUMO)', medName, seriesName, valDate, 0, value]]
     };
 
     var request = gapi.client.sheets.spreadsheets.values.append(params, valueRangeBody);
