6fa149ef38659d8dd37e679cdfc85274f9b8629c
Lucas <lucas@localhost.localdomain>
2019-11-28 23:13:52 -0300

    register functions

--- gsheets.js
+++ gsheets.js
@@ -12,6 +12,7 @@
  var authorizeButton = document.getElementById('authorize_button');
  var signoutButton = document.getElementById('signout_button');
 
+ var SPREADSHEET_ID = '1nLBX1Y-3P_d-RsuD6IeJ1AEZ26IgCm1rQPhthocagZ0';
 
  /**
   *  On load, called to load the auth2 library and API client library.
@@ -90,7 +91,7 @@
  function getMedSeries() {
    var data;
    gapi.client.sheets.spreadsheets.values.get({
-     spreadsheetId: '1nLBX1Y-3P_d-RsuD6IeJ1AEZ26IgCm1rQPhthocagZ0',
+     spreadsheetId: SPREADSHEET_ID,
      range: 'ProdutoLote!A2:D',
    }).then(function(response) {
      data = response.result;
@@ -105,44 +106,80 @@
    });
  }
 
+ function updateMeds(data) {
+  var str='';
+  for (var i=0; i < data.length;++i){
+    str += '<option value="'+data[i]+'" />'; // Storing options in variable
+    }
+  var meds_list=document.getElementById("medicamentSelection");
+  meds_list.innerHTML = str;
+ }
 
- function updateSeriesNames() {
+ function updateSeries(data) {
+  var medName=document.getElementById("medInput").value;
+  console.log(medName)
+  selectedItems = data.filter(function(el) {
+      return el[0] == medName;
+    })
+  console.log(selectedItems)
+  var str='';
+  for (var i=0; i < selectedItems.length;++i){
+    str += '<option value="'+selectedItems[i][1]+'" />'; // Storing options in variable
+  }
+  var seriesList=document.getElementById("seriesSelection");
+  seriesList.innerHTML = str;
+
+  var str2='';
+  for (var i=0; i < selectedItems.length;++i){
+    str2 += '<option data-option='+selectedItems[i][1]+' value="'+selectedItems[i][2]+'" />'; // Storing options in variable
+  }
+  var valList=document.getElementById("valDateSelection");
+  valList.innerHTML = str2;
+ }
+
+
+ function updateForm(data){
+  var barCode=document.getElementById("bar_code").value;
+
+  selectedItems = data.filter(function(el) {
+    return el[0] == barCode;
+  });
+  console.log(selectedItems.length)
+
+  if(selectedItems.length==1){
+    document.getElementById('medInput').value=selectedItems[0][1];
+    document.getElementById('seriesInput').value=selectedItems[0][2];
+    document.getElementById('valInput').value=selectedItems[0][3];
+
+  } else if(selectedItems.length>1) {
+    
+  }
+
+}
+
+
+ function getSheet(sheetRange, callback) {
   var data;
   gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: '1nLBX1Y-3P_d-RsuD6IeJ1AEZ26IgCm1rQPhthocagZ0',
-    range: 'ProdutoLote!A2:D',
+    spreadsheetId: SPREADSHEET_ID,
+    range: sheetRange,
   }).then(function(response) {
     data = response.result;
     if (data.values.length > 0) {
       console.log(data.values)
-      listMedSeries = data.values;
-      var medName=document.getElementById("medInput").value;
-      console.log(medName)
-      selectedItems = listMedSeries.filter(function(el) {
-        return el[0] == medName;
-      })
-    console.log(selectedItems)
-    var str='';
-    for (var i=0; i < selectedItems.length;++i){
-      str += '<option value="'+selectedItems[i][1]+'" />'; // Storing options in variable
-    }
-    var seriesList=document.getElementById("seriesSelection");
-    seriesList.innerHTML = str;
-
-    var str2='';
-    for (var i=0; i < selectedItems.length;++i){
-      str2 += '<option data-option='+selectedItems[i][1]+' value="'+selectedItems[i][2]+'" />'; // Storing options in variable
-    }
-    var valList=document.getElementById("valDateSelection");
-    valList.innerHTML = str2;
-
+      callback(data.values);
+      return data.values;
     } else {
+      alert('Dados não encontrados '+sheetRange)
       appendPre('No data found.');
     }
   }, function(response) {
+    alert('Erro ao carregar '+sheetRange)
     appendPre('Error: ' + response.result.error.message);
   });
  }
+
+
 
 function valSel() {
   var seriesVal=document.getElementById("seriesInput").value;
@@ -156,34 +193,24 @@ function valSel() {
   }
 }
 
- 
- function getMeds() {
-    var data;
-    gapi.client.sheets.spreadsheets.values.get({
-      spreadsheetId: '1nLBX1Y-3P_d-RsuD6IeJ1AEZ26IgCm1rQPhthocagZ0',
-      range: 'Medicamentos!C2:C',
-      majorDimension: 'COLUMNS',
-    }).then(function(response) {
-      data = response.result;
-      if (data.values.length > 0) {
-        console.log(data.values);
-        var str='';
-        var newOptions = data.values[0];
-        for (var i=0; i < data.values[0].length;++i){
-          str += '<option value="'+data.values[0][i]+'" />'; // Storing options in variable
-          }
-        var my_list=document.getElementById("medicamentSelection");
-        my_list.innerHTML = str;
+ function sendReg(){
+  medName=document.getElementById('medInput').value;
+  seriesName=document.getElementById('seriesInput').value;
+  valDate=document.getElementById('valInput').value;
+  value=document.getElementById('qnt').value;
 
-        return data.values[0];
-      } else {
-        appendPre('No data found.');
-      }
-    }, function(response) {
-      appendPre('Error: ' + response.result.error.message);
-    });
-  }
+  addRegister(medName, seriesName, valDate, value)
+ }
 
+ function sendCod(){
+  medName=document.getElementById('medInput').value;
+  seriesName=document.getElementById('seriesInput').value;
+  valDate=document.getElementById('valInput').value;
+  value=document.getElementById('qnt').value;
+  barCode=document.getElementById('bar_code').value;
+  setBarCode(barCode, medName, seriesName, valDate)
+
+ }
   function setBarCode(barCode, medName, seriesName, valDate) {
     var params = {
       spreadsheetId: '1nLBX1Y-3P_d-RsuD6IeJ1AEZ26IgCm1rQPhthocagZ0',  // TODO: Update placeholder value.
@@ -205,7 +232,7 @@ function valSel() {
         // TODO: Change code below to process the `response` object:
         console.log(response.result);
       }, function(reason) {
-        console.error('error: ' + reason.result.error.message);
+        alert('error: ' + reason.result.error.message);
       }
     );
   }
@@ -213,17 +240,17 @@ function valSel() {
   function addRegister(medName, seriesName, valDate, value) {
     var params = {
       spreadsheetId: '1nLBX1Y-3P_d-RsuD6IeJ1AEZ26IgCm1rQPhthocagZ0',  // TODO: Update placeholder value.
-      range: 'Registros!A1:D1',  // TODO: Update placeholder value.
+      range: 'Registros!A1:G',  // TODO: Update placeholder value.
       // How the input data should be interpreted.
       valueInputOption: 'RAW',
       insertDataOption: 'INSERT_ROWS',
     };
 
     var valueRangeBody = {
-      "range": 'Registros!A1:D1',  //Set this to cell want to add 'x' to.
+      "range": 'Registros!A1:G',  //Set this to cell want to add 'x' to.
         "majorDimension": "ROWS",
         "values": [
-          [medName, seriesName, valDate, value]]
+          [dataAtualFormatada(), 'FARMACIA INTERNA', 'SAIDA', medName, seriesName, valDate, value]]
     };
 
     var request = gapi.client.sheets.spreadsheets.values.append(params, valueRangeBody);
@@ -231,8 +258,17 @@ function valSel() {
         // TODO: Change code below to process the `response` object:
         console.log(response.result);
       }, function(reason) {
-        console.error('error: ' + reason.result.error.message);
+        alert('error: ' + reason.result.error.message);
       }
     );
   }
 
+  function dataAtualFormatada(){
+    var data = new Date(),
+        dia  = data.getDate().toString(),
+        diaF = (dia.length == 1) ? '0'+dia : dia,
+        mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+        mesF = (mes.length == 1) ? '0'+mes : mes,
+        anoF = data.getFullYear();
+    return diaF+"/"+mesF+"/"+anoF;
+}
